ce2333b46e8dc2d94bc54e17c66ee06a28bf36c1
Yusuke17 <yuusuke.k1717@gmail.com>
2024-02-07 18:17:45 +0900

    [apdate]施設を追加もしくはレベルを上げる、施設が追加された場合テキストを出す

--- Assets/factory/facility.cs
+++ Assets/factory/facility.cs
@@ -1,49 +1,57 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 [Serializable]
 public class Factory
 {
-	public string FactoryName;
-	public int FactoryLevel;
+	public string factoryName;
+	public int factoryLevel;
 	public float cookiesPerSecond;
-
-	public Factory(string name, int level, float cookie)
+	public float factoryManey;
+	
+	public Factory(string name, int level, float cookie, float NeedManey)
 	{
-		FactoryName = name;
-		FactoryLevel = level;
+		factoryName = name;
+		factoryLevel = level;
 		cookiesPerSecond = cookie;
+		factoryManey = NeedManey;
 	}
 }
 
 public class Facility : MonoBehaviour
 {
 	[SerializeField] List<Factory> factories = new List<Factory>();
-	[SerializeField] Text _texts;
+	//テキストの設定を変更したい場合
+	[SerializeField] private TextMeshProUGUI _factoryText;
 	
-	
-	void AddFactories(string name, float cookiesPerSecond)
+	void AddFactories(string name, float cookiesPerSecond, float needManey)
 	{
-		foreach (var factory in factories)
+		var wherefact = factories.Where(x => x.factoryName == name);
+		foreach (var factory in wherefact)
 		{
-			if (factory.FactoryName == name)
-			{
-				++factory.FactoryLevel;
-			}//施設があった場合レベルと増えるクッキーの値を増やす
-			else
-			{
-				factories.Add(new Factory(name, 1, cookiesPerSecond));
-				Instantiate(_texts, transform);
-			}//なかったらリストに追加してテキストを自分の子オブジェクトに出す
-		}
+			++factory.factoryLevel;
+			factory.cookiesPerSecond *= 2;
+			factory.factoryManey *= 1.15f;
+		}//施設があった場合レベルと増えるクッキーの値を増やす
+
+		if(wherefact.Count() <= 0)
+		{
+			factories.Add(new Factory(name, 1, cookiesPerSecond, needManey));
+			TextMeshProUGUI _text = Instantiate(new GameObject().AddComponent<TextMeshProUGUI>(), transform);
+			_text.text = name;
+		}//なかったらリストに追加してテキストを自分の子オブジェクトに出す
 	}
 
 	private void Start()
 	{
+		//AddFactories("aaa", 1f, 1);
 		StartCoroutine(AddCookieOpe());
 	}
 
