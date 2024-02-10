eac1b7402034ae6784f8fd9cb9146214cb6f8479
showgo-koinuma <van71413989i@gmail.com>
2024-02-10 21:30:21 +0900

    [fix] リソースの増やし方の変更

--- Assets/Resource/ResourceManager.cs
+++ Assets/Resource/ResourceManager.cs
@@ -2,6 +2,9 @@ using System;
 using TMPro;
 using UnityEngine;
 
+/// <summary>
+/// リソースを管理する
+/// </summary>
 public class ResourceManager : MonoBehaviour
 {
     [SerializeField] TMP_Text _resorceText;
@@ -11,6 +14,7 @@ public class ResourceManager : MonoBehaviour
     public Action<ulong> OnResorceChanged;
 
     ulong _resorce = 0ul;
+    float _fraction = 0;
 
     public ulong Resorce
     {
@@ -22,16 +26,30 @@ public class ResourceManager : MonoBehaviour
         }
     }
 
-    public void AddResorce(int value)
+    /// <summary>
+    /// リソースを追加する
+    /// </summary>
+    /// <param name="value"></param>
+    public void AddResorce(float value)
     {
-        Resorce += (ulong)value;
+        _fraction += value;
+        Resorce += (ulong)(_fraction / 1f);
+        _fraction %= 1f;
     }
 
+    /// <summary>
+    /// リソースを消費する
+    /// </summary>
+    /// <param name="value"></param>
     public void UseResorce(int value)
     {
         Resorce -= (ulong)value;
     }
 
+    /// <summary>
+    /// 現在のリソース量をテキストに反映する
+    /// </summary>
+    /// <param name="value"></param>
     void ReflectText(ulong value)
     {
         _resorceText.text = Resorce.ToString();
@@ -41,11 +59,13 @@ public class ResourceManager : MonoBehaviour
     {
         if (_instance) Destroy(gameObject);
         else _instance = this;
+
         OnResorceChanged += ReflectText;
     }
 
     private void Start()
     {
+        // 初期値をテキストに反映
         OnResorceChanged.Invoke(Resorce);
     }
 }
